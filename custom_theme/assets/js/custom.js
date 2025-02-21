304efb7f9236be9c8b9b35882d14a19d9611f6a9
yuanzhenkun <1241367422@qq.com>
2025-02-21 12:01:45 +0800

    feat: 百度统计

---
+++ custom_theme/assets/js/custom.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+  console.log('i am ollama')
+  var _hmt = _hmt || [];
+  (function() {
+    var hm = document.createElement("script");
+    hm.src = "https://hm.baidu.com/hm.js?be62e5d1e814e52f3375a7a6f042b6ff";
+    var s = document.getElementsByTagName("script")[0]; 
+    s.parentNode.insertBefore(hm, s);
+  })();
+})
